4f0e71a2eb0ad5788a971a6a7306987afac9cd81
Damian <damiansuaz@gmail.com>
2025-02-06 17:16:39 -0500

    Start laying out the fillin object

---
+++ docs/index.js
@@ -0,0 +1,32 @@
+const fillInInfo = {
+  name : 'John Doe',
+  title : 'Junior Frontend Developer',
+  contact : {
+    address : {
+      num : '123',
+      street : 'Robot Street'
+    },
+    municipal : {
+      city : 'Principal',
+      state : {
+        stateFullName: 'KS',
+        stateAbbr : 'Kansas'
+      }, 
+      zipCode : '66612'
+  },
+    number : {
+      area : '841',
+      main : '461-3799'
+  },
+    email : 'buckanddoe@gmail.com'
+  },
+  skills : [
+    
+  ],
+  education : {
+  },
+  internet : {
+    website : '',
+    social : ''
+  }
+}
