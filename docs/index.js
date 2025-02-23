a01bfc66acc13244b0ab676db2bc06f41b893cff
Damian <damiansuaz@gmail.com>
2025-02-22 19:23:23 -0500

    Add attributes and sample querySelector

--- docs/index.js
+++ docs/index.js
@@ -21,12 +21,75 @@ const fillInInfo = {
     email : 'buckanddoe@gmail.com'
   },
   skills : [
-    
+    'programming',
+    'reading',
+    'research',
+    'troubleshooting',
+    'critical thinking',
+    'philosophy',
+    'chew gum and walk simultaneously'
   ],
   education : {
+    school : {
+      name: "Cornell Institute",
+      location: "Bismark, WA",
+      start_date: {
+        month: "September",
+        year: 2012
+          },
+      end_date: {
+        month: "June",
+        year: 2016
+          }
+    }
+  },
+  experience : {
+    company : {
+      name : "Fortune 50",
+      location : "Old York, NY",
+      is_present : true,
+      start_date: {
+        month: "August",
+        year: 2016
+      },
+      end_date: {
+        month: "",
+        year: ""
+        },
+      skills: [
+        "money",
+        "discussions",
+        "persuasion",
+        "note-taking"
+        ]
+    },
+company : {
+      name : "Summer Camp",
+      location : "Rio Grande, MS",
+      is_present : false,
+      start_date: {
+        month: "June",
+        year: 2016
+      },
+      end_date: {
+        month: "August",
+        year: "2016"
+        },
+      skills: [
+        "childcare",
+        "organization"
+        ]
+    },
   },
   internet : {
-    website : '',
-    social : ''
+    website : 'google.com',
+    social : 'musk@x.com'
   }
 }
+
+const skillsEle = document.querySelector("#skillslist");
+
+
+window.onload = function(e) {
+  
+};
